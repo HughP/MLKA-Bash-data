d0537769b9c9aee0e6abecac3b3d371a7e1ca5f6
HughP <hugh@thejourneyler.org>
2015-05-26 00:28:03 -0700

    added  dependnecies

--- awesome-script.sh
+++ awesome-script.sh
@@ -51,9 +51,32 @@ echo
 if hash csvfix 2>/dev/null; then
     echo "Great you have csvfix installed."
 else
-    echo "Shucks! You do not have csvfix. You need to get it.  You can use Mercurial and compile it yourself from: https://bitbucket.org/neilb/csvfix
-    echo "Or, you can use Homebrew. 'Brew install csvfix'."
+    echo "Shucks! You do not have csvfix. You need to get it.  You can use Mercurial and compile it yourself from: https://bitbucket.org/neilb/csvfix "
+    echo "Or on OS X if you are using Homebrew you can type: 'Brew install csvfix'."
 fi
+
+# Check to see if TECkit is installed and in path:
+if hash teckit_compile 2>/dev/null; then
+    echo "Great you have teckit_compile installed."
+else
+    echo "Shucks! You do not have teckit_compile. You need to get it.  SIL International is the distributor. It is part of TECkit. Check here: http://scripts.sil.org/TECkitDownloads "
+fi
+
+# Check to see if txtconv is installed and in path:
+if hash UnicodeCCount 2>/dev/null; then
+    echo "Great you have txtconv installed."
+else
+    echo "Shucks! You do not have txtconv. You need to get it.  SIL International is the distributor. It is part of TECkit. Check here: http://scripts.sil.org/TECkitDownloads "
+fi
+
+# Check to see if UnicodeCCount is installed and in path:
+if hash UnicodeCCount 2>/dev/null; then
+    echo "Great you have UnicodeCCount installed."
+else
+    echo "Shucks! You do not have UnicodeCCount. You need to get it.  SIL International is the distributor. Check here: http://scripts.sil.org/UnicodeCharacterCount "
+fi
+
+
 
 ##########
 #Clean up the working folder. Remove files from a previous run of the script. Data folders are processed before they are created.
