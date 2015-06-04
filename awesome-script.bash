1ed1dab557bdb5b3d667d826f29c9c31e1ce26c4
Jonathan Duff <jonathan@dufffamily.org>
2015-06-04 09:39:06 -0700

    moved variables at beginning of awesome-script.bash to global_vars.bash and put an include source statement

--- awesome-script.bash
+++ awesome-script.bash
@@ -37,61 +37,8 @@ echo "      dependencies installed, and looking at the"
 echo "      corpus data on hand."
 echo
 
-##############################
-# Variables for Directories
-##############################
-
-# Variables for UnicodeCCount output
-DIR_INITIAL_STATS_TITLE=Initial-Stats
-DIR_SECOND_STATS_TITLE=Second-Stats
-DIR_THIRD_STATS_TITLE=Third-Stats
-
-# Variables for Corpora versions
-DIR_JAMES_DATA=James-Data # This variable needs to be updated in the clean-up script. I wish there was a way to reference these variables from that script.
-DIR_WIKI_DATA=Wiki-Data
-DIR_TYPOGRAHICAL_CORRECT_DATA=Typographically-Clean-Corpora
-DIR_CLEAN_AND_POSSIBLE_DATA=Typo-Clean-And-Possible-To-Type-Corpora
-DIR_TEC_FILES=TECkit-tec-Files
-
-##############################
-# Variables for File Names Prefixes
-##############################
-
-INITIAL_STATS_TITLE=First_Stats
-SECOND_STATS_TITLE=Second_Stats
-THIRD_STATS_TITLE=Third_Stats
-
-INS_TRANSPOSED=First_Stats_Transposed # Used for transposed CSV files
-
-##############################
-# Variables for Other Things
-##############################
-
-# List of the file names of the Data files (corpora and keyboards).
-
-JAMES_LIST_FILE=James-list.txt #This is a file list of the James Corpus files.
-WIKI_LIST_FILE=Wikipedia-list.txt #This file contains the file names of the Wikipedia data dumps. 
-# OTHER_LIST_FILE= # This is for other corpora to be added in a later version. (some where around v. 0.9.8)
-CORPUS_LIST_FILE=Corpus-list.txt #This file is a list of all corpora (James + Wikipedia # + other)
-KEYBOARD_LIST_FILE_FP=Full-Path-Keyboard-list.txt # This file lists all the keyboard files with their full path relative to the home directory. Included are .kmx, .keylayout, .kmn, (and perhaps more) other blocks which reference this file need to take into account that there are multiple file types in this file.
-KEYBOARD_LIST_FILE=Keyboard-list.txt #This file lists all the keyboard files. Included are .kmx, .keylayout, .kmn, (and perhaps more) other blocks which reference this file need to take into account that there are multiple file types in this file.
-
-# List of all languages used in the data processing
-LANGUAGE_LIST_FILE=Language_ID.txt # This file is for all languages, not just one of the three arrays.
-CORPORA_LANGUAGES=Corpora_Languages.txt
-JAMES_LANGUAGES=James_Languages.txt
-WIKI_LANGUAGES=Wikipedia_Languages.txt
-OTHER_CORPORA_LANGUAGES=
-KEYBOARDS_LANGUAGES=Keyboard_Languages.txt
-
-CMD_UNICODECCOUNT=UnicodeCCount
-
-KEYBOARD_FILE_TYPES=../Keyboard-File-Types/Keyboard-File-Types.txt # This file is externally maintained and imported to help this application determine if there are keyboard file types which need to be searched for.
-
-DATA_TYPE= # This should be an array made dynamically from various attributes of the data types. We have Keyboards, and each type of corpora. This array should motivate the tables in the display output.
-CORPUS_TYPE= # This needs to be dynamically determined and then added to an array. Should be like Data_type but only an array.
-
-
+# Grab global variables:
+source global_vars.bash
 
 ##############################
 # Dependencies Checks
@@ -456,7 +403,7 @@ if [ -d "$DIR_JAMES_DATA" ]; then
 	    # http://www.tldp.org/LDP/abs/html/dblparens.html
         fi
     done
-# Report what was found and moved.    
+# Report what was found and moved.
     echo "INFO: Moved " $JAMES_DATA_FILE_COUNT " James texts into the $DIR_JAMES_DATA folder."
 	echo
 fi
@@ -513,8 +460,8 @@ KBD_FILE_TYP_str=$(csvfix read_dsv -f 1 -s '\t' $KEYBOARD_FILE_TYPES | sed 's/\"
 KBD_FILE_TYP=${KBD_FILE_TYP_str//'\t'/'\n'}
 
 for i in $KBD_FILE_TYP; do
- 	find * -iname \*."${i}" | sort -t. -k 2,2 -k 1,1 >> $KEYBOARD_LIST_FILE_FP 
-done	   
+ 	find * -iname \*."${i}" | sort -t. -k 2,2 -k 1,1 >> $KEYBOARD_LIST_FILE_FP
+done
 
 # Then make a list of just the file names of the keyboards
 cat $KEYBOARD_LIST_FILE_FP | rev |cut -d '/' -f1 | rev >> $KEYBOARD_LIST_FILE
@@ -759,7 +706,7 @@ content = open('/Users/Hugh/wiki_00.txt')
 
 
 """ try to set the path variable with the following:
-import os 
+import os
 path = os.getcwd()
 
 """
@@ -975,21 +922,21 @@ for i in $(cat "$INS_TRANSPOSED"-list-csv.txt);do
 #  remove some extra line in the file.
 #  ingest each line as a seperate array?
 #  echo each array in the format in the pyfile template for pygal?
-# 
+#
 #  OR
-# 
+#
 #  Hand craft the first pygal SVG chart and then turn this SVG into a CSV file via csvfix XML command. Then create the SVG template file.
-# 
+#
 #  import pygal                                                       # First import pygal
 # from pygal.style import BlueStyle
 # chart = pygal.StackedLine(fill=True, interpolate='cubic', style=BlueStyle)
-# 
+#
 # bar_chart = pygal.Bar()                                            # Then create a bar graph object
 # bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])  # Add some values
 # bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])  # Add some values
 # bar_chart.render_to_file('bar_chart.svg')                          # Save the svg to a file
-# 
-# 
+#
+#
 # FIND a SVG to .jpg or .png application. See discussion here: http://superuser.com/questions/134679/command-line-application-for-converting-svg-to-png-on-mac-os-x
 
 
