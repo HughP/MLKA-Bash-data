48a89773c4e763db71d633227cd72ed4997f2a4d
Jonathan Duff <jonathan@dufffamily.org>
2015-06-09 01:58:25 -0700

    TMP_JD/dependencies.bash almost there, check it out

--- TMP_JD/dependencies.bash
+++ TMP_JD/dependencies.bash
@@ -2,13 +2,25 @@
 
 #-smq
 
-column1=( $(csvfix read_dsv -csv -s '|' -f 1 dependencies.data) )
-column2=( $(csvfix read_dsv -csv -s '|' -f 2 dependencies.data) )
-column3=( $(csvfix read_dsv -csv -s '|' -f 3 dependencies.data) )
+column1=($(csvfix read_dsv -csv -s '|' -f 1 dependencies.data | tr '\n' ' '))
+column2=($(csvfix read_dsv -csv -s '|' -f 2 dependencies.data | tr '\n' ' '))
+column3=($(csvfix read_dsv -csv -s '|' -f 3 dependencies.data | tr '\n' ' '))
 
 for (( i=0; i < ${#column1[*]}; i++ )); do
-            echo "1=" ${column1[$i]}
-            echo "2=" ${column2[$i]}
-            echo "3=" ${column3[$i]}
+            echo "row=" $i
+            echo "column1=" ${column1[$i]}
+            echo "column2=" ${column2[$i]}
+            echo "column3=" ${column3[$i]}
             echo
+done
+
+echo
+echo
+echo
+
+myarray=("this has spaces" "this has spaces also")
+
+for (( i=0; i < ${#myarray[*]}; i++)); do
+    echo "item=" $i
+    echo "myarray=" ${myarray[$i]}
 done
