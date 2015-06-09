a74223f2d7d73381faf0c5b6591d1d50d444e1f8
Jonathan Duff <jonathan@dufffamily.org>
2015-06-09 00:40:27 -0700

    working the dependencies section

--- TMP_JD/dependencies.bash
+++ TMP_JD/dependencies.bash
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+#-smq
+
+column1=( $(csvfix read_dsv -csv -s '|' -f 1 dependencies.data) )
+column2=( $(csvfix read_dsv -csv -s '|' -f 2 dependencies.data) )
+column3=( $(csvfix read_dsv -csv -s '|' -f 3 dependencies.data) )
+
+for (( i=0; i < ${#column1[*]}; i++ )); do
+            echo "1=" ${column1[$i]}
+            echo "2=" ${column2[$i]}
+            echo "3=" ${column3[$i]}
+            echo
+done
