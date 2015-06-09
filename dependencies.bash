b98bf367d2b074d4996a5aaef7a9154464086f41
HughP <hugh@thejourneyler.org>
2015-06-09 00:36:04 -0700

    solution to dependencies

--- dependencies.bash
+++ dependencies.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-x=$(csvfix read_dsv -smq -s "\t" -f 1 dependencies.data)
-y=$(csvfix read_dsv -smq -s "\t" -f 2 dependencies.data)
-z=$(csvfix read_dsv -smq -s "\t" -f 3 dependencies.data)
 
-for i in ${x[*]}; do
-    echo "X=" ${x[$i]}
-    echo "Y=" ${y[$i]}
-    echo "Z=" ${z[$i]}
+
+for i in dependencies.data; do
+x=($(csvfix read_dsv -csv -smq -s '|' -f 1 dependencies.data))
+y=($(csvfix read_dsv -csv -smq -s '|' -f 2 dependencies.data))
+z=($(csvfix read_dsv -csv -smq -s '|' -f 3 dependencies.data))
+
+ for i in ${x[@]}; do
+    echo "X=" ${x}
+    echo "Y=" ${y}
+    echo "Z=" ${z}
 done
-
+done
 
 #    if type $x &>/dev/null; then
 #        echo
