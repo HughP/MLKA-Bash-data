a9bbd2a961993a550e2f5d3a01654a2c60b41c4d
HughP <hugh@thejourneyler.org>
2015-06-10 23:10:30 -0700

    checking out jonathans solution. works nicely made it fit in my own code.

--- dependencies.bash
+++ dependencies.bash
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-
-
+IFS=$'\n'
 for i in dependencies.data; do
-x=($(csvfix read_dsv -csv -smq -s '|' -f 1 dependencies.data | ))
-y=($(csvfix read_dsv -csv -smq -s '|' -f 2 dependencies.data | ))
-z=($(csvfix read_dsv -csv -smq -s '|' -f 3 dependencies.data | ))
+x=($(csvfix read_dsv -csv -smq -s '|' -f 1 dependencies.data ))
+y=($(csvfix read_dsv -csv -smq -s '|' -f 2 dependencies.data ))
+z=($(csvfix read_dsv -csv -smq -s '|' -f 3 dependencies.data ))
 
- for i in ${x[@]}; do
-    if type ${x} &>/dev/null; then
-        echo
-        printf ${y[*]}
-        echo
-    else
-    	echo
-        printf "${z[*]}"
-        echo
-        exit 1
-    fi
+unset $IFS
+	for (( i=0; i < ${#x[@]}; i++ )); do
+	    if type ${x} &>/dev/null; then
+	        echo
+	        echo ${y[$i]}
+	        echo
+	    else
+	    	echo
+	        echo ${z[$i]}
+	        echo
+	        exit 1
+	    fi
+	done
 done
-done
-
-#done
